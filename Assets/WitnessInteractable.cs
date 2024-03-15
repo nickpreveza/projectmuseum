2d6c1327f55f83c253652e38aa75ab22fe800146
Nick A. Spyrakos <nickpreveza@gmail.com>
2024-03-15 18:23:13 +0100

    witness stuff

---
+++ Assets/WitnessInteractable.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class WitnessInteractable : MonoBehaviour
+{
+    //generate grid?
+    //line renderer 
+    //lines ui, has to be in the world too 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
